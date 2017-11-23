8086ec416ee46ec81c2ad56c7564977dde244977
Noa Avigad <noa3179@gmail.com>
2017-11-22 21:24:31 -0800

    added location input request and let lon matching

--- TranslinkAPI.hs
+++ TranslinkAPI.hs
@@ -1,9 +1,33 @@
 -- API Key: qwRjMjGRk9PMqJklKOPK
 
 
+-- temporary solution until we implement fetching of current location
+getUserLocation = [49.279171, -122.919808]
 
--- stack script --resolver lts-8.22
-{-# LANGUAGE OverloadedStrings #-}
+-- getLatLon returns [Lat,Lon] accordingly, if "my location" is passed in, the current location is fetched
+-- if location passed in is unfamiliar, return an empty array
+getLatLon :: [Char] -> [Double]
+getLatLon poi
+    | poi == "ICICS" = [49.260986, -123.248064]
+    | poi == "city hall" = [49.263248, -123.114934]
+    | poi == "nest" = [49.266502, -123.249666]
+    | poi == "BCIT" = [49.249149, -123.001068]
+    | poi == "SFU" = [49.279171, -122.919808]
+    | poi == "my location" = getUserLocation
+    | otherwise = []
+
+
+main = do
+putStrLn "Please enter a location:"
+pointOfInterest <- getLine
+-- get lat lon of the entered poi
+let latLon = getLatLon pointOfInterest
+putStrLn $ show (head latLon)
+putStrLn $ show (latLon!!1)
+
+
+
+{- just for reference - ignore for now
 import qualified Data.ByteString.Lazy.Char8 as L8
 import           Network.HTTP.Simple
 
@@ -15,19 +39,4 @@ putStrLn $ "The status code was: " ++
         show (getResponseStatusCode response)
 print $ getResponseHeader "Content-Type" response
 L8.putStrLn $ getResponseBody response
-
-
-
-
-
-
-
-
---Accept'='application/json
-
-
-
--- let opts = defaults & header "Authorisation" .~ ["AUTH_INFO_STRING"]
--- & header "custom-header" .~ ["string"]
--- & param  "search_term"   .~ ["my","search"]
--- getWith opts "http://httpbin.org/someUrl"
+-}
